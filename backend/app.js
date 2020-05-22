6f36362c84a499ade5687b3032a46874ce863954
Matt Carpenter <mattcarpenter@gmail.com>
2020-05-23 07:46:26 +0900

    remove cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var apiRouter = require('./routes/api');
@@ -10,7 +9,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
