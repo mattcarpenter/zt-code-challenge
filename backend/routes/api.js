6b488d7f4ca9d6ac44f8d556a3e97dda02413a1d
Matt Carpenter <mattcarpenter@gmail.com>
2020-05-22 20:38:54 +0900

    api request validation

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 const unsplashService = require('../services/unsplash');
 const config = require('../config');
 
-router.get('/search', async (req, res) => {
+router.get('/search', [
+  check('query').exists()
+], async (req, res) => {
+
+  // request validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors });
+  }
+
+  // perform image search and respond with result
   const results = await unsplashService.search('kittens', config.getUnsplashAccessKey());
   res.send(results);
 });
