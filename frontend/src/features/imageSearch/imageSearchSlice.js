58c316a6b5071285118fd3dbc94dd8678edd84f1
Matt Carpenter <mattcarpenter@gmail.com>
2020-05-23 15:56:44 +0900

    enhance image search slice tests

--- frontend/src/features/imageSearch/imageSearchSlice.js
+++ frontend/src/features/imageSearch/imageSearchSlice.js
@@ -5,14 +5,15 @@ export const initialState = {
   images: [],
   page: null,
   total: null,
-  totalPages: null
+  totalPages: null,
+  query: null
 };
 
 export const searchImages = createAsyncThunk(
   'imageSearch/searchImages',
   async ({query, page = 1, perPage = 10}) => {
     const response = await axios.get('/api/mock', {params: { query, page, perPage } });
-    return response.data;
+    return { ...response.data, page: page, query: query };
   }
 );
 
@@ -25,7 +26,15 @@ export const imageSearchSlice = createSlice({
     [searchImages.fulfilled]: (state, action) => {
       state.total = action.payload.total;
       state.totalPages = action.payload.total_pages;
-      state.images = state.images.concat(action.payload.results);
+
+      if (action.payload.query === state.query) {
+        // same query; append images
+        state.images = state.images.concat(action.payload.results);
+      } else {
+        // new query; replace images
+        state.query = action.payload.query;
+        state.images = action.payload.results;
+      }
     }
   }
 });
--- frontend/src/features/imageSearch/imageSearchSlice.test.js
+++ frontend/src/features/imageSearch/imageSearchSlice.test.js
@@ -5,17 +5,6 @@ import moxios from 'moxios';
 
 const mockStore = configureStore([thunk]);
 
-const mockAxiosResponse = {
-  status: 200,
-  response: {
-    total: 2,
-    total_pages: 2,
-    results: [
-      {}, {}
-    ]
-  }
-};
-
 describe('imageSearch slice', () => {
   let store;
 
@@ -35,22 +24,45 @@ describe('imageSearch slice', () => {
     expect(result).toEqual(nextState);
   });
 
-  it('dispatch the correct actions after a successful image search', async () => {
+  it('dispatches the correct actions after a successful image search', async () => {
+    const mockAxiosResponse = createMockImageSearchResponse(100, 2, 50);
     moxios.stubRequest(/.*/, mockAxiosResponse);
 
-    await store.dispatch(searchImages({query: 'kittens '}));
+    await store.dispatch(searchImages({query: 'kittens'}));
     const actions = store.getActions();
 
     expect(actions[0].type).toBe(searchImages.pending.type);
     expect(actions[1].type).toBe(searchImages.fulfilled.type);
-    expect(actions[1].payload).toBe(mockAxiosResponse.response);
+    expect(actions[1].payload).toStrictEqual({ ...mockAxiosResponse.response, query: 'kittens', page: 1 });
   });
 
-  it('should properly add the images and pagination information when an image search succeeds', () => {
-    const nextState = reducer(initialState, searchImages.fulfilled(mockAxiosResponse.response));
-    expect(nextState.images).toEqual(mockAxiosResponse.response.results);
-    expect(nextState.total).toEqual(mockAxiosResponse.response.total);
-    expect(nextState.totalPages).toEqual(mockAxiosResponse.response.total_pages);
+  it('properly adds images and pagination information when images searches succeed', () => {
+    const mockAxiosResponse = createMockImageSearchResponse(100, 2, 50);
+    const state2 = reducer(initialState, searchImages.fulfilled({ ...mockAxiosResponse.response, query: 'kittens'}));
+    expect(state2.images).toEqual(mockAxiosResponse.response.results);
+    expect(state2.total).toEqual(mockAxiosResponse.response.total);
+    expect(state2.totalPages).toEqual(mockAxiosResponse.response.total_pages);
+
+    // simulate another search with the same query; expect new results to be added to previous results
+    const state3 = reducer(state2, searchImages.fulfilled({ ...mockAxiosResponse.response, query: 'kittens'}));
+    expect(state3.images.length).toBe(100);
+
+    // simulate another search with a different query; expect the new results to replace the previous results
+    const mockAxiosResponse2 = createMockImageSearchResponse(75, 1, 75);
+    const state4 = reducer(state3, searchImages.fulfilled({ ...mockAxiosResponse2.response, query: 'puppies'}));
+    expect(state4.images.length).toBe(75);
+    expect(state4.query).toBe('puppies');
   });
 
 });
+
+function createMockImageSearchResponse(total, totalPages, itemCount) {
+  return {
+    status: 200,
+    response: {
+      total: total,
+      total_pages: totalPages,
+      results: [...Array(itemCount).keys()].map(k => {})
+    }
+  };
+}
