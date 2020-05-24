eee1408c08cdffa5d71a83aeef7defb02521da8e
Matt Carpenter <mattcarpenter@gmail.com>
2020-05-24 23:15:45 +0900

    Navigation bar tests

--- frontend/src/features/core/NavigationBar/NavigationBar.test.js
+++ frontend/src/features/core/NavigationBar/NavigationBar.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import { mount } from 'enzyme';
+import NavigationBar from './NavigationBar';
 
 describe('NavigationBar', () => {
 
   it('renders correctly', () => {
-
+    const wrapper1 = mount(<NavigationBar navIndex={0} />);
+    const wrapper2 = mount(<NavigationBar navIndex={1} />);
+    expect(wrapper1).toMatchSnapshot();
+    expect(wrapper2).toMatchSnapshot();
   });
 
 });
