f494ba057e810fa8cd6639f889f9b22eb800cac3
Matt Carpenter <mattcarpenter@gmail.com>
2020-05-24 19:40:42 +0900

    persist favorites and lists to localStorage

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import imageSearchReducer from '../features/imageSearch/imageSearchSlice';
 import favoritesReducer from '../features/favorites/favoritesSlice';
+import persistState from 'redux-localstorage';
 
 export default configureStore({
   reducer: {
     imageSearch: imageSearchReducer,
     favorites: favoritesReducer
   },
+  enhancers: [
+    persistState('favorites')
+  ]
 });
